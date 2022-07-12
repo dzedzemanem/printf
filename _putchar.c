882a461045f8a0f8c3b8e161e2987a4d59238c10
dzedzemanem <105044808+dzedzemanem@users.noreply.github.com>
2022-07-12 21:57:54 +0200

    retry

--- _putchar.c
+++ _putchar.c
@@ -1,11 +1,13 @@
 #include <unistd.h>
+
 /**
  * _putchar - writes the char to c stdout
  * @c: the character to print
  *
  * Return: write system call. -1 if something went wrong.
  */
+
 int _putchar(char c)
 {
-return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
