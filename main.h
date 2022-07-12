49befae1451f5c7b7d095e2d861cbb071dee480d
dzedzemanem <dzedzemanem@gmail.com>
2022-07-12 13:17:57 -0700

    mf

---
+++ main.h
@@ -0,0 +1,36 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * struct printer - format printer struct
+ * @spec: the format specifier
+ * @fn: the function that handles @spec
+ */
+
+typedef struct printer
+{
+  char *spec;
+  int (*fn)(va_list);
+} print_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list ap);
+int print_string(va_list ap);
+int print_space(va_list ap);
+int print_int(va_list ap);
+int print_unsigned(va_list ap);
+int print_octal(va_list ap);
+int print_digit(int num, int *count);
+int print_unsigned_digit(unsigned int num, int *count);
+int print_digit_octal(unsigned int num, int *count);
+int call_print_fn(char ch, va_list ap);
+
+#endif 
