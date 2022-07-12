489aeb000ffdc5e0761aeb4ab0a93ed1720fc1ee
dzedzemanem <dzedzemanem@gmail.com>
2022-07-12 12:10:54 -0700

    md

--- _printf.c
+++ _printf.c
@@ -1,40 +1,47 @@
 #include "main.h"
+#include <limits.h>
 #include <stdio.h>
 /**
- * _printf - produces output accrding to format
- * @format: character string composed of zero or more directives
- *
- * Return: the number of characters printed
- * (excluding the null byte used to end output to strings)
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
+int (*pfunc)(va_list, flags_t *);
+const char *p;
 va_list arguments;
-unsigned int sum = 0;
-unsigned int n = 0;
-if (format == NULL)
-{
-return (-1);
-}
+flags_t flags = {0, 0, 0};
+register int count = 0;
 va_start(arguments, format);
-for (; format[n] != '\0'; n++)
-{
-if (format[n] == '\0' || (format[n] == '%' && !format[n + 1]))
-{
+if (!format || (format[0] == '%' && !format[1]))
 return (-1);
-}
-else if (format[n] == '%' && (format[n + 1] == 'd' ||
-format[n + 1] == 'i' || format[n + 1] == 's' ||
-format[n + 1] == 'c' || format[n + 1] == '%'))
+if (format[0] == '%' && format[1] == ' ' && !format[2])
+return (-1);
+for (p = format; *p; p++)
 {
-sum += (*converter(format[n + 1]))(arguments);
-n++;
+if (*p == '%')
+{
+p++;
+if (*p == '%')
+{
+count += _putchar('%');
+continue;
+}
+while (get_flag(*p, &flags))
+p++;
+pfunc = get_print(*p);
+count += (pfunc)
+? pfunc(arguments, &flags)
+: _printf("%%%c", *p);
 }
 else
-{
-sum += _putchar(format[n]);
+count += _putchar(*p);
 }
-}
+_putchar(-1);
 va_end(arguments);
-return (sum);
+return (count);
 }
